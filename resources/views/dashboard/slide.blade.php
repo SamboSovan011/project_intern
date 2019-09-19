3e33dda92f40e85b1f35ceadad144a91c45b0a34
SamboSovan011 <lucksolent@gmail.com>
2019-09-19 16:50:16 +0700

    Add Slide feature

--- resources/views/dashboard/slide.blade.php
+++ resources/views/dashboard/slide.blade.php
@@ -1,5 +1,54 @@
 @extends('dashboard.app')
 @section('title', 'Potted Pan - Slide Adder')
 @section('content')
+<style>
+    #form{
+        padding-top: 10rem;
+    }
+</style>
+<section class="content-header">
+    <h1>
+        General Form Elements
+        <small>Preview</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Forms</a></li>
+        <li class="active">General Elements</li>
+    </ol>
+</section>
+<section id="form">
+        <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Slide For Banner</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Title</label>
+                      <input name="title" type="text" class="form-control" id="exampleInputEmail1" placeholder="Slide Title">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Description</label>
+                      <input name="desc" type="text" class="form-control" id="exampleInputPassword1" placeholder="Description">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputFile">File input</label>
+                      <input type="file" id="exampleInputFile">
+
+                      <p class="help-block">Example block-level help text here.</p>
+                    </div>
+
+                  </div>
+                  <!-- /.box-body -->
+
+                  <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </form>
+              </div>
+</section>
 
 @endsection
